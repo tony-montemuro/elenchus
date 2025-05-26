e5909136975eb5fc11ba951ed7ec414699b7cece
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-26 17:48:19 -0500

    improve middleware readability

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,11 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"slices"
+)
+
+type chain []func(http.Handler) http.Handler
+
+// func (c chain) thenFunc(h http.HandlerFunc) http.Handler {
+// 	return c.then(h)
+// }
+
+func (c chain) then(h http.Handler) http.Handler {
+	for _, mw := range slices.Backward(c) {
+		h = mw(h)
+	}
+
+	return h
+}
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", app.home)
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	globalChain := chain{app.recoverPanic, app.logRequest, commonHeaders}
+	return globalChain.then(mux)
 }
