379c2b020bf6c6fdee565f2fc54daba4dcad0fb9
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-30 06:31:32 -0500

    setup basic file server for serving static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,6 +22,9 @@ func (c chain) then(h http.Handler) http.Handler {
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /ping", ping)
 
