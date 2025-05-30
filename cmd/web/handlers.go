94aee6fbab15f42669d7847b619984ef24e7b968
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-29 21:00:50 -0500

    add basic template rendering from home route

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
