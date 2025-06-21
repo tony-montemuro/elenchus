76a5d3e0c5cb8d70619a9dea31d2701de5be8c10
tony.montemuro <tonyamontemuro@gmail.com>
2025-06-21 11:04:00 -0500

    session -> sessions migration fix

--- migrations/20250611200658-create_sessions_table.sql
+++ migrations/20250611200658-create_sessions_table.sql
@@ -1,13 +1,13 @@
 
 -- +migrate Up
-CREATE TABLE session (
+CREATE TABLE sessions (
     token CHAR(43) PRIMARY KEY,
     data BLOB NOT NULL,
     expiry TIMESTAMP(6) NOT NULL
 );
 
-CREATE INDEX session_expiry_idex ON session (expiry);
+CREATE INDEX sessions_expiry_idex ON sessions (expiry);
 
 -- +migrate Down
-DROP INDEX session_expiry_idex ON session;
-DROP TABLE session;
+DROP INDEX sessions_expiry_idex ON sessions;
+DROP TABLE sessions;
