d9f3d6e1f0133441b7f942350d56baddb2671557
tony.montemuro <tonyamontemuro@gmail.com>
2025-06-24 07:02:10 -0500

    create answers model

---
+++ internal/models/answers.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Answer struct {
+	ID      int
+	Content string
+	Correct bool
+	Created time.Time
+	Update  time.Time
+	Deleted time.Time
+}
+
+type AnswerModel struct {
+	DB *sql.DB
+}
